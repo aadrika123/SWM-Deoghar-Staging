0c86a098bbe6b4c091cb92e9ca9a1007db5e0f3c
akshay2s <akshay.aadrika@gmail.com>
2024-05-24 10:55:47 +0530

    chnages

---
+++ react-server.js
@@ -0,0 +1,28 @@
+// successfull react serve working code for react
+const path = require("path");
+const express = require("express");
+const app = express(); // create express app
+
+console.log('before hitt')
+//home route will redirect to the citizen-pannel route
+app.get("/", (req, res) => {
+  console.log('home route hitted')
+  res.redirect('/swm')
+});
+
+//middleware to server static files also
+app.use(express.static("swm_build"));
+
+
+//actual routes
+app.get("/swm", (req, res) => {
+  res.sendFile(path.join(__dirname, "swm_build", "index.html"));
+});
+app.get("/swm/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "swm_build", "index.html"));
+});
+
+// start express server on port 80
+app.listen(600, () => {
+  console.log("server starting on port 500");
+});
