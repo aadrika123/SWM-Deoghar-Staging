b33e3d73bbddc7b5ac9060c6e8347081955289df
rohan8883 <rohansingh9135@gmail.com>
2024-07-01 18:43:13 +0530

    new changes

---
+++ deoghar_app_build/assets/RotatingLoader-CabBhJS-.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-B4EgP_SM.js";import{$ as r}from"./module-nEn-CVxc.js";function s(){return t.jsx("div",{className:"flex justify-center items-center",children:t.jsx(r,{strokeWidth:"4",strokeColor:"#6b7280",width:"37"})})}export{s as R};
