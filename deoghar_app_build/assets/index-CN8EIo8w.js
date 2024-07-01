f78e2585b4dae3baaacc5482ce51e25e96bfb0eb
rohan8883 <rohansingh9135@gmail.com>
2024-07-01 17:05:41 +0530

    new changes

---
+++ deoghar_app_build/assets/index-CN8EIo8w.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Cn5xDO1F.js";import"./router-DQohkvhc.js";import"./headlessui-qWRqSxHB.js";import"./axios-DmypytPv.js";function s(){return r.jsx("div",{children:"Register"})}export{s as default};
