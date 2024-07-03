46d83344e4c3fcf9c9f0969f36c0d1fabb2ec9d6
rohan8883 <rohansingh9135@gmail.com>
2024-07-03 16:25:11 +0530

    new changes

---
+++ deoghar_app_build/assets/ButtonLoading-pUx7Qzv7.js
@@ -0,0 +1,1 @@
+import{j as o,R as n}from"./index-C3grFowZ.js";import{B as r}from"./button-DJxuGp-P.js";function i({isLoading:t,children:s,...a}){return o.jsxs(r,{...a,disabled:t,children:[t&&o.jsx(n,{className:"mr-2 h-4 w-4 animate-spin"}),t?"Loading...":s]})}export{i as B};
--- deoghar_app_build/assets/RotatingLoader-VY-0V7Qf.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DuK7PHPF.js";import{$ as r}from"./module-Blw9N4d7.js";function s(){return t.jsx("div",{className:"flex justify-center items-center",children:t.jsx(r,{strokeWidth:"4",strokeColor:"#6b7280",width:"37"})})}export{s as R};
