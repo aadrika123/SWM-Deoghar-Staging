94fe1f5ae2e6be77f4b60764424d7ad0eb9c396d
rohan8883 <rohansingh9135@gmail.com>
2024-07-03 14:07:35 +0530

    new chnages

---
+++ deoghar_app_build/assets/ButtonLoading-BZCtEzYY.js
@@ -0,0 +1,1 @@
+import{j as o,R as n}from"./index-DuK7PHPF.js";import{B as r}from"./button-BI-NVyGK.js";function i({isLoading:t,children:s,...a}){return o.jsxs(r,{...a,disabled:t,children:[t&&o.jsx(n,{className:"mr-2 h-4 w-4 animate-spin"}),t?"Loading...":s]})}export{i as B};
--- deoghar_app_build/assets/RotatingLoader-BLr2CIKK.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BcSDAYQy.js";import{$ as r}from"./module-CmHYFK_O.js";function s(){return t.jsx("div",{className:"flex justify-center items-center",children:t.jsx(r,{strokeWidth:"4",strokeColor:"#6b7280",width:"37"})})}export{s as R};
