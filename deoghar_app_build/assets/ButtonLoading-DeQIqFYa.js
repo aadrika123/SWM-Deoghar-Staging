04d3cbc19c7055951fcaaabf3e576c395c0d3111
rohan8883 <rohansingh9135@gmail.com>
2024-07-06 17:40:50 +0530

    new changes

---
+++ deoghar_app_build/assets/ButtonLoading-DeQIqFYa.js
@@ -0,0 +1,1 @@
+import{j as t,B as n,R as o}from"./index-BlhGWuhU.js";function m({isLoading:s,children:a,...e}){return t.jsxs(n,{...e,disabled:s,children:[s&&t.jsx(o,{className:"mr-2 h-4 w-4 animate-spin"}),s?"Please wait...":a]})}export{m as B};
