7b2cf38347a15ff8ee8e480a6782a1d4fca13e02
rohan8883 <rohansingh9135@gmail.com>
2024-07-06 11:58:19 +0530

    newc hanges

--- deoghar_app_build/assets/RotatingLoader-CCrgefjq.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CuInqoRq.js";import{$ as r}from"./module-CZOxRCJ5.js";function s(){return t.jsx("div",{className:"flex justify-center items-center",children:t.jsx(r,{strokeWidth:"4",strokeColor:"#6b7280",width:"37"})})}export{s as R};
