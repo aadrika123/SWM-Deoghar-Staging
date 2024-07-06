10d30de9f9fcb572acfb42d513c5f8627aaf3a77
rohan8883 <rohansingh9135@gmail.com>
2024-07-06 18:56:56 +0530

    new changes

--- deoghar_app_build/assets/RotatingLoader-pqzSNpfj.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-2N1J2mcs.js";import{$ as r}from"./module-TRWR25lQ.js";function s(){return t.jsx("div",{className:"flex justify-center items-center",children:t.jsx(r,{strokeWidth:"4",strokeColor:"#6b7280",width:"37"})})}export{s as R};
--- deoghar_app_build/assets/index-C9795jch.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-2N1J2mcs.js";function g({page:t,perPage:n,totalPage:a,setPage:o}){return e.jsx(e.Fragment,{children:null})}export{g as P};
