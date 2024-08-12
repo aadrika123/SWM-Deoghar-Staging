5dcf1d36af8429909f190913f45364575add2e5f
rohan8883 <rohansingh9135@gmail.com>
2024-08-12 12:12:58 +0530

    new changes

--- deoghar_app_build/assets/ButtonLoading-BCOZsAPy.js
+++
@@ -1,1 +0,0 @@
-import{j as t,B as n,R as o}from"./index-dMd2Y3Xj.js";function m({isLoading:s,children:a,...e}){return t.jsxs(n,{...e,disabled:s,children:[s&&t.jsx(o,{className:"mr-2 h-4 w-4 animate-spin"}),s?"Please wait...":a]})}export{m as B};
