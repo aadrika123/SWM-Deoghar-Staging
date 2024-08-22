f52f509fbe3eda5ee83523c94d6d61388d1ccc13
rohan8883 <rohansingh9135@gmail.com>
2024-08-22 13:40:25 +0530

    build for both mobile app and web view

--- deoghar_app_build_old/assets/RotatingLoader-D3mWH3Zx.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-D3K5dCwF.js";import{$ as r}from"./module-B--5YRvA.js";function s(){return t.jsx("div",{className:"flex justify-center items-center",children:t.jsx(r,{strokeWidth:"4",strokeColor:"#6b7280",width:"37"})})}export{s as R};
--- deoghar_app_build_old/assets/index-DdXtyljU.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-D3K5dCwF.js";function g({page:t,perPage:n,totalPage:a,setPage:o}){return e.jsx(e.Fragment,{children:null})}export{g as P};
