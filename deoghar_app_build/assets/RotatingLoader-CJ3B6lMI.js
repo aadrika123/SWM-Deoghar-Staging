1e6084940daaea7da3b8b6c181716a95903bf97d
rohan8883 <rohansingh9135@gmail.com>
2024-09-20 17:16:28 +0530

    new changes

---
+++ deoghar_app_build/assets/RotatingLoader-CJ3B6lMI.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DskiXWfo.js";import{$ as r}from"./module-CwN8NruV.js";function s(){return t.jsx("div",{className:"flex justify-center items-center",children:t.jsx(r,{strokeWidth:"4",strokeColor:"#6b7280",width:"37"})})}export{s as R};
